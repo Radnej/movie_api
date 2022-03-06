4c9b5b77473ab60de310d6ff7a6466f3521a6d63
Radnej <91905344+Radnej@users.noreply.github.com>
2022-03-06 14:25:30 +0100

    edit logging with morgen/edited

--- index.js
+++ index.js
@@ -5,17 +5,10 @@ const app = express();
 const morgan = require('morgan');
 
 // Logging with Morgen
-morgan = require('morgan');
-
-const app = express();
-
 app.use(morgan('common'));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to myFlix!');
-});
+// List Top 10 Movies
 
-// Create array of objects that holds top 10 movies
 let topMovies = [
     {
       name: 'Licorice Pizza',
@@ -78,8 +71,11 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
+  
 
   // Listen to port 8080
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
-  });
+});
+
+
