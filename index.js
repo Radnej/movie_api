1ea686d15565bbfb4b4f4e6f96e7c377b8a789ff
Radnej <91905344+Radnej@users.noreply.github.com>
2022-04-29 22:40:36 +0200

    mongoose added

--- index.js
+++ index.js
@@ -1,106 +1,180 @@
 const express = require('express'),
 bodyParser = require('body-parser'),
-uuid = require('uuid'),
-morgan = require('morgan');
+uuid = require('uuid');
 
+const morgan = require('morgan');
 const app = express();
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+//mongoose models
+const Movies = Models.Movie;
+const Users = Models.User;
+
+//connect to MongoDB
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { 
+  userNewUrlParser: true, 
+  useUnifiedTopology: true, 
+});
 
 app.use(bodyParser.json());
 
-let users = [
-  {
-    username: 1,
-    favoritMovies: ['Licorice Pizza', 'Scream']
-  },
-  {
-    username: 2,
-    favoritMovies: ['Licorice Pizza']
-
-  }
-];
-let movies = [
-  {
-  name: 'Licorice Pizza',
-  year: 2022,
-  genre: {
-  name: 'Comedy',
-  description:'LICORICE PIZZA is the story of Alana Kane and Gary Valentine growing up, running around and falling in love in the San Fernando Valley, 1973. Written and Directed by Paul Thomas Anderson, the film tracks the treacherous navigation of first love.',
-},
-  director: { 
-    name:'Paul Thomas Anderson',
-    bio:'Anderson was born in 1970. He was one of the first of the "video store" generation of film-makers.',
-    birth:1970
-  },
-  imageURL:'https://www.imdb.com/title/tt11271038/?ref_=adv_li_i',
-  featured: true
-},
-{
-  name: 'Scream ',
-  year: 2021,
-  genre: {
-    name: 'Horror',
-    description:'Twenty-five years after the original series of murders in Woodsboro, a new Ghostface emerges, and Sidney Prescott must return to uncover the truth.',
-},
-  director: { 
-    name:'Matt Bettinelli-Olpin',
-    bio:'Matt Bettinelli-Olpin is a director and writer, known for Scream (2022), Ready or Not (2019) and Southbound (2015).',
-    birth:''
-  },
-  imageURL:'https://www.imdb.com/title/tt11245972/mediaviewer/rm3414551553/?ref_=tt_ov_i',
-  features: true
-}
-];
+// default text response when at /
+app.get('/', (req, res) => {
+  res.send('Welcome to MyFlix!');
+});
 
 //Return list of ALL movies to the user
 app.get('/movies', (req, res) => {
-  res.send('Show director name');
+  Movie.find().then((movies) => {
+    res.status(200).json(movies);
+  });
 });
 
 
 //CREAT user in the app
 app.post('/users', (req, res) => {
-  res.send('Add user');
+  Users.findOne({ Username: req.body.Username }).then((user) => {
+  if(user) {
+    return res.status(400).send('${req.body.Username} already exist');
+  } else {
+    Users.create({
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday,
+    })
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send('Error: ${error}');
+    });
+  }
+  });
 });
 
 //Update change username
-app.put('/users/:username', (req, res) => {
-  const newUsername = req.body;
-  res.send('Update user by username');
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true},
+    (err, updatedUser) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error: ${err}');
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  )
 });
 //CREAT add movie to user favorit list 
-app.post('/users/:username/:movie', (req, res) => {
-  res.send('Add movie');
+app.post('/users/:Username/favorites/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: {
+        FavoritMovies: req.params.MovieID,
+      },
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error: ${err}');
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
 
 //DELET  remove movie from user favorits  list 
-app.delete('/users/:username/:movie', (req, res) => {
-  res.send('delet movie');
+app.delete('/users/:Username/favorites/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $pull: {
+        FavoritMovies: req.params.MovieID,
+      },
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error: ${err}');
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
 
 //DELET  User  
-app.delete('/users/:username', (req, res) => {
-  res.send('delet user');
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({Username: req.params.Username})
+  .then((user) =>{
+    if (!user) {
+      return res.status(400).send('${req.params.Username} does not exist');
+    } else {
+      res.status(200).send('${req.params.Username} was deleted')
+    }
+  }).catch((err) =>{
+    console.log(err);
+    res.status(500).send('Error: ${err}');
+  })
 });
 
 
 
 //READ Movies Name
 app.get('/movies/:name', (req, res) => {
-  res.send('Show movie name');
+  Movies.findOne({ Name: req.params.name }).then((movie) => {
+    if (movie) {
+      res.status(200).json(movie);
+    } else {
+      res.status(400).send('Movie not Found');
+    }
+  });
 });
 
 //READ Movies Genre
 app.get('/movies/geners/:name', (req, res) => {
-  res.send('Show movie genre');
+  Movie.findOne({ Name: req.params.name}).then((movie) => {
+    if (movie) {
+      res.status(200).send( '${req.params.name} is a ${movie.Genre.Name}');
+    } else {
+      res.status(400).send('Movie not Found');
+    }
+  });
 });
 
 //READ Movies Director
 app.get('/movies/directors/:name', (req, res) => {
-  res.send('Show director name');
+  Movie.findOne({'Director.Name': req.params.name}).then((movie) => {
+    if (movie) {
+      res.status(200).json(movie.Director);
+    } else {
+      res.status(400).send('Director not Found');
+    }
+  });
 });
 
 
 // Serve static content for the app from the 'public' directory
+app.use(morgan('common'));
+app.use(express.json());
 app.use(express.static('public'));
 
 // Error handling (middleware)
