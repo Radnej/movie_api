5ec66cde2cda56ca513d759d757f137f37783772
Radnej <91905344+Radnej@users.noreply.github.com>
2022-05-01 17:54:20 +0200

    add delete user endpoint

--- index.js
+++ index.js
@@ -150,6 +150,22 @@ app.post('/users', (req, res) => {
     });
 });
 
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 // Error handling (middleware)
 app.use((err, req, res, next) => {
   console.error(err.stack);
