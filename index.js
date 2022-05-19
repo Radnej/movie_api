ffdbc245ddbe081588f520896f4d9d92e4a388bf
Radnej <91905344+Radnej@users.noreply.github.com>
2022-05-19 21:40:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 
-//let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://my-flix-220508.herokuapp.com/' , 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://my-flix-220508.herokuapp.com/' , 'http://localhost:1234'];
 
- // app.use(cors({
-  //  origin: (origin, callback) => {
-  //    if(!origin) return callback(null, true);
-  //   if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-  //      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
-  //      return callback(new Error(message ), false);
-  //    }
-  //    return callback(null, true);
-  //  }
-  //}));
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //connect to MongoDB
 
